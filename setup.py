708406003f81cf9f129ff7d223f62122c787b655
Lee Crosby <ligand@hotmail.co.uk>
2020-09-18 20:11:00 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+# https://github.com/microsoft/vscode-recipes/tree/master/debugging%20python
+
+exec(open("./crony/manifest.py").read())
+manifest = locals().get('__manifest__')
+
+setup(
+    name = manifest['pkgname'],
+    version = manifest['version'],
+    author = "Lee Crosby",
+    author_email = "lee.crosby@outlook.com",
+    description = manifest['description'],
+    keywords = "development workflow server maintenance",
+    packages = find_packages(),
+    install_requires = [
+        'croniter',
+        'python-crontab',
+        'packaging',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'crony = crony.cli:main'
+        ]
+    }
+)
