5f914e779ceef86e874416dbe2580ac0ddefa8e0
lee <lee@feeditback.co.uk>
2020-09-22 17:33:28 +0100

    split into 'ui' and something central..

--- crony/cli.py
+++ crony/cli.py
@@ -2,16 +2,12 @@ import sys
 import os
 import logging
 from datetime import datetime
-from enum import Enum
 import argparse
 
-from crontab import CronTab
-
-import crony.analyser
+import crony.core
 import crony.manifest
 from crony.levelledoption import LevelledOption
 
-# todo: split files up
 # todo: missing docblocks
 
 # todo: print another format which is just all the datetimes each job would have run.
@@ -22,8 +18,6 @@ from crony.levelledoption import LevelledOption
 # todo: test all options
 
 # todo: travis unit tests + badge
-
-
 
 # travis ci:
 # follow:
@@ -43,23 +37,16 @@ from crony.levelledoption import LevelledOption
 
 _logger = logging.getLogger(__name__)
 
-_DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-
 _LOG_LEVELS = LevelledOption('v', [
     'WARNING',
     'INFO',
     'DEBUG'
 ], default='ERROR')
 
-class DetailLevel(Enum):
-    NONE = 0
-    COUNT = 1
-    FULL = 2
-
 _DETAIL_LEVELS = LevelledOption('d', [
-    DetailLevel.COUNT.name,
-    DetailLevel.FULL.name
-], default=DetailLevel.NONE.name)
+    crony.core.DetailLevel.COUNT.name,
+    crony.core.DetailLevel.FULL.name
+], default=crony.core.DetailLevel.NONE.name)
 
 # From https://stackoverflow.com/questions/25470844/specify-format-for-input-arguments-argparse-python
 def _valid_datetime(s):
@@ -75,38 +62,15 @@ def _valid_datetime(s):
         datetime.datetime: The parsed datetime
     """
     try:
-        return datetime.strptime(s, _DEFAULT_DATE_FORMAT)
+        return datetime.strptime(s, crony.core.DEFAULT_DATE_FORMAT)
     except ValueError:
         raise argparse.ArgumentTypeError(f"Invalid datetime: '{s}'.")
 
-def parse_crontab(pargs):
-    """Parse crontab-related args
-
-    Args:
-        pargs (dict): The parsed arguments
-
-    Returns:
-        tuple: A tuple of (human readable crontab source, crontab.CronTab)
-    """
-    if sys.stdin.isatty():
-        _input = pargs.get('input')
-        _user = pargs.get('user')
-        if _input:
-            return ('input', CronTab(tabfile=_input))
-        elif _user:
-            return (f"user:{_user}", CronTab(user=_user))
-        else:
-            # In the case of none of them being passed, we default to the current user.
-            return ('user:current', CronTab(user=True))
-    else:
-        # If we're not a tty, then try to read a crontab from stdin.
-        return ('stdin', CronTab(tab=sys.stdin.read()))
-
-def _init_logging(pargs):
+def _init_logging(args):
     """Initialise logging
 
     Args:
-        pargs (dict): The parsed args
+        args (dict): The parsed args
     """
     root_logger = logging.getLogger()
 
@@ -117,55 +81,7 @@ def _init_logging(pargs):
     root_logger.addHandler(handler)
 
     # Set the root/global log level:
-    root_logger.setLevel(_LOG_LEVELS.parse(pargs))
-
-def _stringize_datetime(dt):
-    """Convert a datetime to the default format used
-
-    Args:
-        dt (datetime.datetime): A datetime object to format
-
-    Returns:
-        str: The formatted datetime
-    """
-    return dt.strftime(_DEFAULT_DATE_FORMAT)
-
-def _build_header(pargs):
-    """Build the header
-
-    Args:
-        pargs (dict): The parsed args
-    """
-    return f"For {pargs['source']}: {_stringize_datetime(pargs['begin'])} -> {_stringize_datetime(pargs['end'])}"
-
-def _run(pargs):
-    """Run the program based on parsed args
-
-    Args:
-        pargs (dict): The parsed args
-    """
-    # Parse args:
-    (pargs['source'], pargs['crontab']) = parse_crontab(pargs)
-
-    # Print the header if not excluded:
-    if not pargs['exclude_header']:
-        print(_build_header(pargs))
-        print()
-
-    # Get the detail level, the default is None, so no detail.
-    detail_level = DetailLevel[_DETAIL_LEVELS.parse(pargs)]
-
-    # Find jobs occurring in the provided datetime range:
-    for job in crony.analyser.get_job_occurrences(**pargs):
-        # Print the command / whole line based on provided options
-        print(job.command if pargs['only_command'] else job.line)
-
-        if detail_level.value >= DetailLevel.COUNT.value
-            print(f"\tOccurrences: {len(job.occurrences)}")
-
-        if detail_level.value >= DetailLevel.FULL.value:
-            for occurrence in job.occurrences:
-                print("\t\t" + _stringize_datetime(occurrence))
+    root_logger.setLevel(_LOG_LEVELS.parse(args))
 
 def main():
     """The application entry point
@@ -198,9 +114,9 @@ def main():
         parser.add_argument('--only-command', '-c', action='store_true', help="Only show the command, not the full line")
         _DETAIL_LEVELS.add_to_parser(parser, 'Output at the {level} level')
 
-        pargs = vars(parser.parse_args())
-        _init_logging(pargs)
-        _run(pargs)
+        parsed_args = vars(parser.parse_args())
+        _init_logging(parsed_args)
+        crony.core.run(**parsed_args)
 
     except KeyboardInterrupt:
         sys.exit(0)
---
+++ crony/core.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import logging
+from datetime import datetime
+from enum import Enum
+
+from crontab import CronTab
+
+import crony.analyser
+
+_logger = logging.getLogger(__name__)
+
+DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+class DetailLevel(Enum):
+    NONE = 0
+    COUNT = 1
+    FULL = 2
+
+def _parse_crontab(**kwargs):
+    """Parse crontab-related args
+
+    Args:
+        kwargs (dict): Keyword args
+
+    Returns:
+        tuple: A tuple of (human readable crontab source, crontab.CronTab)
+    """
+    if sys.stdin.isatty():
+        _input = kwargs.get('input')
+        _user = kwargs.get('user')
+        if _input:
+            return ('input', CronTab(tabfile=_input))
+        elif _user:
+            return (f"user:{_user}", CronTab(user=_user))
+        else:
+            # In the case of none of them being passed, we default to the current user.
+            return ('user:current', CronTab(user=True))
+    else:
+        # If we're not a tty, then try to read a crontab from stdin.
+        return ('stdin', CronTab(tab=sys.stdin.read()))
+
+def _stringize_datetime(dt):
+    """Convert a datetime to the default format used
+
+    Args:
+        dt (datetime.datetime): A datetime object to format
+
+    Returns:
+        str: The formatted datetime
+    """
+    return dt.strftime(DEFAULT_DATE_FORMAT)
+
+def _build_header(**kwargs):
+    """Build the header
+
+    Args:
+        kwargs (dict): Keyword args
+    """
+    return f"For {kwargs['source']}: {_stringize_datetime(kwargs['begin'])} -> {_stringize_datetime(kwargs['end'])}"
+
+def run(**kwargs):
+    """Run the program based on kwargs
+
+    Args:
+        kwargs (dict): Keyword args
+    """
+    # Parse args:
+    (kwargs['source'], kwargs['crontab']) = _parse_crontab(kwargs)
+
+    # Print the header if not excluded:
+    if not kwargs['exclude_header']:
+        print(_build_header(**kwargs))
+        print()
+
+    # Get the detail level, the default is None, so no detail.
+    detail_level = DetailLevel[_DETAIL_LEVELS.parse(kwargs)]
+
+    # Find jobs occurring in the provided datetime range:
+    for job in crony.analyser.get_job_occurrences(**kwargs):
+        # Print the command / whole line based on provided options
+        print(job.command if kwargs['only_command'] else job.line)
+
+        if detail_level.value >= DetailLevel.COUNT.value
+        print(f"\tOccurrences: {len(job.occurrences)}")
+
+        if detail_level.value >= DetailLevel.FULL.value:
+            for occurrence in job.occurrences:
+                print("\t\t" + _stringize_datetime(occurrence))
