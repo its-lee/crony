14a61841699a863234888d17e40297f2ba92063f
lee <lee@feeditback.co.uk>
2020-09-25 13:34:00 +0100

    tests prototype

---
+++ tests/test_analyser.py
@@ -0,0 +1,9 @@
+import unittest
+
+class AnalyserTest(unittest.TestCase):
+
+    def test_success(self):
+        self.assertEqual(15, 15)
+
+    def test_fail(self):
+        self.assertEqual(15, -15)
