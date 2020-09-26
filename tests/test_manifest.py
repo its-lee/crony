dda53307b6d5a4c3e48c97260a6bfbd521798f51
Lee Crosby <ligand@hotmail.co.uk>
2020-09-27 00:23:06 +0100

    Create test_manifest.py

---
+++ tests/test_manifest.py
@@ -0,0 +1,10 @@
+import unittest
+from crony import manifest
+
+class ManifestTest(unittest.TestCase):
+
+    def test_success(self):
+        self.assertEqual('1.0.0', manifest.version)
+
+    #def test_fail(self):
+    #    self.assertEqual(15, -15)
