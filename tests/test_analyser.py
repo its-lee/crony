3b831dcd32773dd55b66f8d4d3edb3084731c79d
Lee Crosby <ligand@hotmail.co.uk>
2020-09-27 00:52:56 +0100

    something for the analyser

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -1,9 +1,21 @@
+import sys
+import os
+from datetime import datetime
+
 import unittest
+from crontab import CronTab
+
+import crony.analyser
+
+def to_datetime(s, fmt=None):
+    return datetime.strptime(s, fmt if fmt else "%Y-%m-%d %H:%M:%S")
 
 class AnalyserTest(unittest.TestCase):
 
-    def test_success(self):
-        self.assertEqual(15, 15)
-
-    #def test_fail(self):
-    #    self.assertEqual(15, -15)
+    def test_basic(self):
+        occurrences = cron.analyser.get_job_occurrences(
+            crontab=CronTab(tab="* * * * * hiiiii"),
+            begin=to_datetime('2020-01-01 00:00:00'),
+            end=to_datetime('2020-01-02 00:00:00')
+        )
+        # TODO: set up some parameterised cases, and edge cases. the only testing done so far catches exceptions
