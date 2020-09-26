e07500a2fec6029832eedb166031b37cfdaceec9
Lee Crosby <ligand@hotmail.co.uk>
2020-09-27 00:24:46 +0100

    break

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -4,7 +4,7 @@ from crony import manifest
 class ManifestTest(unittest.TestCase):
 
     def test_success(self):
-        self.assertEqual('1.0.0', manifest.version)
+        self.assertEqual('1.0.1', manifest.version)
 
     #def test_fail(self):
     #    self.assertEqual(15, -15)
