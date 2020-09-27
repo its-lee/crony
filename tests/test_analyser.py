30fb9f8c5f95971db0d25e070003494b86e7728a
Lee Crosby <ligand@hotmail.co.uk>
2020-09-27 18:44:38 +0100

    Update test_analyser.py

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -14,11 +14,11 @@ class AnalyserTest(unittest.TestCase):
 
     def test_basic(self):
         occurrences = crony.analyser.get_job_occurrences(
-            crontab=CronTab(tab="
+            crontab=CronTab(tab="""
                 * * * * * hiiiii
                 #* * * * * byeeeee
                 1 2 bad_line
-            "),
+            """),
             begin=to_datetime('2020-01-01 00:00:00'),
             end=to_datetime('2020-01-02 00:00:00'),
             include_disabled=True
