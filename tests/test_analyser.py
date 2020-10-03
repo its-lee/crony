e3f9f689ade13cb0b6f0023412ba99359a42c0b1
Cygnut <ligand@hotmail.co.uk>
2020-10-03 23:30:05 +0100

    fix tests

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -8,6 +8,18 @@ from parameterized import parameterized
 
 import crony.analyser
 
+# A reminder on the cron format:
+#
+# ┌───────────── minute (0 - 59)
+# │ ┌───────────── hour (0 - 23)
+# │ │ ┌───────────── day of the month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                   7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * * <command to execute>
+
 def to_datetime(s, fmt=None):
     return datetime.strptime(s, fmt if fmt else "%Y-%m-%d %H:%M:%S")
 
@@ -25,7 +37,7 @@ class AnalyserTest(unittest.TestCase):
         ("before begin boundary",   "59 23 * * *", 0),
         ("on end boundary",         "30 0 * * *", 1),        
         ("after end boundary",      "31 0 * * *", 0),
-        ("really out of schedule",  "59 11 0 0 0", 0),
+        ("really out of schedule",  "59 11 1 0 0", 0),
         ("commented every minute",  "#* * * * *", 31)
     ])
     def test_single_line(self, _, schedule, expected_occurrence_count):
@@ -60,3 +72,4 @@ class AnalyserTest(unittest.TestCase):
             for o in job.occurrences:
                 str(o)
         # TODO: finish this test, and edge cases. the only testing done so far catches exceptions
+        # TODO: Handle this - "59 11 0 0 0" which is an invalid cron line - we should probably update the code to emit this better
