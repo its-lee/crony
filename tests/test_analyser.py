1ff28a3eaa966a30a38618eb97330990b634126b
Cygnut <ligand@hotmail.co.uk>
2020-10-03 19:31:52 +0100

    basic test

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -11,21 +11,21 @@ def to_datetime(s, fmt=None):
     return datetime.strptime(s, fmt if fmt else "%Y-%m-%d %H:%M:%S")
 
 def get_job_occurrences(lines, **kwargs):
-    return crony.analyser.get_job_occurrences(
+    return list(crony.analyser.get_job_occurrences(
         CronTab(tab="\n".join(lines)), 
         **kwargs
-        )
+        ))
 
 class AnalyserTest(unittest.TestCase):
 
     def test_basic(self):
         jobs = get_job_occurrences([
-            '* * * * * enabled',
-            '#* * * * * disabled',
-            'invalid_line'
+                '* * * * * enabled',
+                '#* * * * * disabled',
+                'invalid_line'
             ],
             begin=to_datetime('2020-01-01 00:00:00'),
-            end=to_datetime('2020-01-01 00:00:30'),
+            end=to_datetime('2020-01-01 00:30:00'),
             include_disabled=False      # Gets more coverage
         )
 
@@ -35,3 +35,17 @@ class AnalyserTest(unittest.TestCase):
             for o in job.occurrences:
                 str(o)
         # TODO: set up some parameterised cases, and edge cases. the only testing done so far catches exceptions
+
+    def test_even_more_basic(self):
+        jobs = get_job_occurrences([
+                '* * * * * enabled',
+            ],
+            begin=to_datetime('2020-01-01 00:00:00'),
+            end=to_datetime('2020-01-01 00:30:00'),
+            include_disabled=False      # Gets more coverage
+        )
+
+        self.assertEquals(1, len(jobs))
+        job = jobs[0]
+        self.assertEquals('enabled', job.command)
+        self.assertEquals(30, len(job.occurrences))
