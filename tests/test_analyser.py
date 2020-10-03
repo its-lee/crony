7761a8b0c0d6d287b151bc9166623bb0e3a65078
Cygnut <ligand@hotmail.co.uk>
2020-10-03 23:19:42 +0100

    never

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -38,7 +38,8 @@ class AnalyserTest(unittest.TestCase):
 
     @parameterized.expand([
         ("every minute", "* * * * *", 4),      # 00, 01, 02, 03
-        ("on the hour", "0 * * * *", 1)
+        ("on the hour", "0 * * * *", 1),
+        ("out of schedule", "59 11 0 0 0", 0)
     ])
     def test_single_line(self, _, schedule, expected_occurrence_count):
         expected_command = "it"
