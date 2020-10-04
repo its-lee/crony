fdb08e879c0fb81be499b745aeab48a8b9c4e324
Cygnut <ligand@hotmail.co.uk>
2020-10-04 21:07:57 +0100

    oops

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,7 +13,7 @@ simple_filepath = write_temp_crontab("* * * * * woof")
 # which aren't very testable.
 class CliTest(unittest.TestCase):
     @parameterized.expand([
-        param("simple", f"--file={simple_filepath} --begin=\"2020-01-01 00:00:00\" --end=\"2020-02-01 01:23:45\"" ])
+        param("simple", f"--file={simple_filepath} --begin=\"2020-01-01 00:00:00\" --end=\"2020-02-01 01:23:45\"")
     ])
     def test_cli_doesnt_die(self, _, args):
         cli.main(shlex.split(args))
