995978cd0707f7688bd5025348aa96e72529e78d
Cygnut <ligand@hotmail.co.uk>
2020-10-04 20:10:23 +0100

    try hitting the cli

---
+++ tests/test_cli.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import unittest
+
+from crony import cli
+
+def _write_temp_crontab(tab):
+    filepath = "/tmp/test-crontab-" + datetime.timestamp(datetime.now())
+    with open(filepath, "w") as f:
+        f.write(tab)
+    return filepath
+
+class CliTest(unittest.TestCase):
+    def test_cli_doesnt_die(self):
+        filepath = _write_temp_crontab("* * * * * woof")
+
+        cli.main({
+            "file": filepath,
+            "begin": datetime(2020, 1, 1, 0, 0, 0, 0),
+            "end": datetime(2020, 2, 1, 0, 0, 0, 0)
+        })
