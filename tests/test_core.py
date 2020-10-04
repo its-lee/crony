2a1df662dec88dcba520e044ff243012e58a4908
Cygnut <ligand@hotmail.co.uk>
2020-10-04 22:23:32 +0100

    for coverage

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,7 +8,11 @@ from crony import core
 
 from tests.util import write_temp_crontab, to_datetime
 
-simple_filepath = write_temp_crontab([ "* * * * * woof" ])
+simple_filepath = write_temp_crontab([ 
+    "* * * * * woof",
+    "0 0 1 1 0 should_rarely_run"    # We want this for coverage, to show a line shouldn't be emitted in that case.
+])
+
 simple_args = { "file": simple_filepath, "begin": "2020-01-01 00:00:00", "end": "2020-02-01 01:23:45" }
 
 class CoreTest(unittest.TestCase):
@@ -16,7 +20,9 @@ class CoreTest(unittest.TestCase):
     @parameterized.expand([
         param("detail level none", { **simple_args, "detail_level": core.DetailLevel.NONE }),
         param("detail level count", { **simple_args, "detail_level": core.DetailLevel.COUNT }),
-        param("detail level full", { **simple_args, "detail_level": core.DetailLevel.FULL })
+        param("detail level full", { **simple_args, "detail_level": core.DetailLevel.FULL }),
+
+        param("has a job which won't run", { "file": simple_filepath, "begin": "2020-01-01 01:23:45", "end": "2020-01-01 01:24:45" }),
     ])
     def test_core_doesnt_die(self, _, kwargs):
         # Convert to actual datetime objects
@@ -26,6 +32,7 @@ class CoreTest(unittest.TestCase):
 
         # Ensure defaults are present, and allow them to be overriden in the same way as on the command line.
         base_kwargs = {
+            "detail_level": core.DetailLevel.NONE,
             "include_disabled": False,
             "exclude_header": False,
             "only_command": False
