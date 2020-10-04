4148c9e8676d759185e92fc3832d21f81762f4a8
Cygnut <ligand@hotmail.co.uk>
2020-10-04 17:56:30 +0100

    test datetimes

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,5 +1,6 @@
 import argparse
 import unittest
+import datetime
 
 from parameterized import parameterized, param
 
@@ -7,8 +8,10 @@ from crony import args
 
 class ArgsTest(unittest.TestCase):
     # TODO: More tests here
+    # TODO: Test defaults
     @parameterized.expand([
-        param("log level",   [ '--vvv' ], { 'log_level': 'DEBUG' }),
+        param("log level",  [ '--vvv' ], { 'log_level': 'DEBUG' }),
+        param("begin",      [ '--begin="2020-01-02 01:23:45"' ], { 'begin': datetime.datetime(2020, 1, 2, 1, 23, 45, 0) }),
     ])
     def test_args(self, _, args_, expected):
         actual = args.parse(args_)
