688f9c676ff708a8e2a8be75f6aa063a9ddf0e68
Cygnut <ligand@hotmail.co.uk>
2020-10-04 22:25:49 +0100

    woops

--- tests/util.py
+++ tests/util.py
@@ -4,7 +4,7 @@ import os
 def write_temp_crontab(lines):
     filepath = "/tmp/test-crontab-" + str(datetime.timestamp(datetime.now()))
     with open(filepath, "w") as f:
-        f.write(lines.join(os.linesep))
+        f.write(os.linesep.join(lines))
     return filepath
 
 def to_datetime(s, fmt=None):
