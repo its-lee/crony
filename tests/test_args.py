3a2db975559c76003f7c242a80b56eb0d2b9ad1a
Cygnut <ligand@hotmail.co.uk>
2020-10-04 16:29:25 +0100

    nope, not that ez

--- tests/test_args.py
+++ tests/test_args.py
@@ -10,16 +10,10 @@ class ArgsTest(unittest.TestCase):
     @parameterized.expand([
         param("no default, no value",   [ '--vvv' ], { 'log_level': 'DEBUG' }),
     ])
-    def test_args(self, _, args_, expected_args):
-        parsed_args = args.parse(args_)
+    def test_args(self, _, args_, expected):
+        actual = args.parse(args_)
 
         # We're happy to just assert that the expected args are contained
-        # in the actual ones. This can be done via dict <=
-        # 
-        # From https://stackoverflow.com/questions/21058230/how-to-assert-a-dict-contains-another-dict-without-assertdictcontainssubset-in-p
-        #
-        # users = {'id': 28027, 'email': 'chungs.lama@gmail.com','created_at': '2005-02-13'}
-        # data = {"email": "chungs.lama@gmail.com"}
-        # self.assertGreaterEqual(user.items(), data.items())
-
-        self.assertGreaterEqual(parsed_args, expected_args)
+        # in the actual ones.
+        subset = { k:v for k, v in actual.items() if k in expected }
+        self.assertDictEqual(subset, expected)
