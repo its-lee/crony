62e0bcb5257e88574681b36d43ddfceea3feb18a
lee <lee@feeditback.co.uk>
2022-05-09 11:32:29 +0100

    1.1 with more short args for convenience

--- crony/manifest.py
+++ crony/manifest.py
@@ -1,5 +1,5 @@
 pkgname = "crony"
-version = "1.0.2"
+version = "1.1.0"
 description = """
 A simple command line program for reporting on crontab job schedules in a provided datetime interval.
 """
