46611ec4b973530f3bf4178d6802adb50a48fa9b
lee <lee@feeditback.co.uk>
2022-05-09 11:41:29 +0100

    1.1 newlines are being removed in future

--- crony/manifest.py
+++ crony/manifest.py
@@ -1,5 +1,3 @@
 pkgname = "crony"
 version = "1.1.0"
-description = """
-A simple command line program for reporting on crontab job schedules in a provided datetime interval.
-"""
+description = "A simple command line program for reporting on crontab job schedules in a provided datetime interval."
